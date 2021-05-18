f5b595ef06f0c3cd264014448fffef02d3c13109
Gabriel Leandro <50453714+GabrielLeandroSilva@users.noreply.github.com>
2021-05-18 17:52:08 -0300

    Create the project and the header

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Jobs/click-musical/admin/');
+define('HTTP_CATALOG', 'http://localhost/Jobs/click-musical/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Jobs/click-musical/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Jobs/click-musical/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/Jobs/click-musical/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/Jobs/click-musical/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/Jobs/click-musical/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/Jobs/click-musical/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/Jobs/click-musical/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/Jobs/click-musical/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/Jobs/click-musical/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'click_musical.mysql.dbaas.com.br');
+define('DB_USERNAME', 'click_musical');
+define('DB_PASSWORD', 'Click@7735*');
+define('DB_DATABASE', 'click_musical');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Jobs/click-musical/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Jobs/click-musical/');
+
+// DIR 
+define('DIR_APPLICATION', 'C:/xampp/htdocs/Jobs/click-musical/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/Jobs/click-musical/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/Jobs/click-musical/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/Jobs/click-musical/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/Jobs/click-musical/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/Jobs/click-musical/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/Jobs/click-musical/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'click_musical.mysql.dbaas.com.br');
+define('DB_USERNAME', 'click_musical');
+define('DB_PASSWORD', 'Click@7735*');
+define('DB_DATABASE', 'click_musical');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
